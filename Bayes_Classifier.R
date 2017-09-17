1ea77ccb57dbec57f086827a212dd4b0036c136c
Guilherme Passos <guilhermepassos@outlook.com>
2017-09-17 13:43:28 -0300

    Update Bayes_Classifier.R

--- Bayes_Classifier.R
+++ Bayes_Classifier.R
@@ -6,10 +6,10 @@
 #    This is an alternative implementation of Naive Bayes Classifier in R. Even though there are many implementations
 # of this algorithm on internet and in most of programming languages (R has its own as well), I decided to develop
 # my onw using the classical Iris data set in order to get a strong understading of this method. Our goal here is 
-# to create a algorithm that is able to sepaate the class SETOSA from the othrer two ones. For a previous 
-# understanding of the Naive Bayes Classifier, check this following lecture here: 
+# to create an algorithm that is able to sepaate the class SETOSA from the othrer two ones. For a previous 
+# understanding of Naive Bayes Classifier, check this following lecture here: 
 # http://software.ucv.ro/~cmihaescu/ro/teaching/AIR/docs/Lab4-NaiveBayes.pdf
-# p.s: This is NOT a general implementation of the Naive Bayes Classifier
+# p.s: This is NOT a general implementation of Naive Bayes Classifier
 
 ###############################
 
